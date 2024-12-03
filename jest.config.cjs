bb5667473102946a91e24bfc29765ebfc2059f41
lenfromkits <lenfromkits@gmail.com>
2024-12-02 21:45:30 -0800

    first commit

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/__tests__/**/*.(ts|tsx)', '**/?(*.)+(spec|test).(ts|tsx)'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+};
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export function GetText(): string {
+    return "Hello World!";
+}
+
+export function Add(a: number, b: number): number {
+    return a + b;
+}
